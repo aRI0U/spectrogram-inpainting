8a94ca0c29ce45dcca6a871e119952fbad524868
Jeffery25 <durand.jeffery@gmail.com>
2021-01-14 17:50:06 +0100

    added convnet decoder

--- code/model/decoders/convnet_decoder.py
+++ code/model/decoders/convnet_decoder.py
@@ -1,118 +1,115 @@
 import torch.nn as nn
 
-
-class ConvNetDecoder(nn.Module):
+class DeconvModel(nn.Module):
+    
     """
-        Creates a parametric Convnet model for the encoder.
+        Creates a parametric DeconvNet model for the decoder.
 
         The model has the following architecture:
-
-        One convolution layer per element of parameter conv_channels:
-            - # of channels given by the item in conv_channels
-            - each convolution is followed by activation conv_activation
-            - maxpool2D between each pair of convolutions
 
         One dense layer per element of parameter dense_layers:
             - # of neurons given by the item in dense_layers
             - each layer is followedby activation dense_activation
+
+        One convolution transpose layer per element of parameter conv_channels:
+            - # of channels given by the item in conv_channels
+            - each convolution is followed by activation conv_activation
+            - UpsamplingNearest2d between each pair of convolutions
     """
 
+
     def __init__(
-            self,
+        self,
 
-            # the dimension of the input pictures and
-            # of the expected output latent variable
-            # (default values correspond to MNIST)
-            input_height=28,
-            input_width=28,
-            input_channels=1,
-            output_dim=10,
+        # the dimension of the input pictures and
+        # of the expected output latent variable
+        # (default values correspond to MNIST)
+        input_dim=10,        
+        output_height=28, 
+        output_width=28,
+        output_channels=1,
 
-            # architecture of the conv and dense layers
-            conv_channels=None,
-            dense_layers=None,
-            conv_kernel=3,
-            maxpool_kernel=2,
+        # architecture of the conv and dense layers
+        dense_layers=None,
+        conv_channels=None, 
+        conv_kernel=3,
+        upsample_factor=2,
 
-            # internal activation layers
-            conv_activation='ReLU',
-            dense_activation='ReLU'
+        # internal activation layers
+        dense_activation='ReLU',
+        conv_activation='ReLU'
     ):
-
+        
         # nn.Module instantiate
         super().__init__()
 
         # store useful parameters
-        self._height = input_height
-        self._width = input_width
-        self._channels = input_channels
+        self._input = input_dim
+
+        # find the dimensions of the first convolution
+        conv_dimensions = [(output_height, output_width)]
+        height, width = output_height, output_width
+        for i in range(len(conv_channels or []) - 1):
+            height = height // upsample_factor ** 2
+            width = width // upsample_factor ** 2
+            conv_dimensions.append((height, weight))
+        conv_dimensions.reverse()
+        print(conv_dimensions[0])
+        self._conv_in = (conv_channels[0], *conv_dimensions[0])
+
+
+        # list linear layers
+        dense_sequence = []
+        widths = [input_dim] + (dense_layers or [])
+        for i in range(1, len(widths)):
+
+            # add a dense layer and activation
+            dense_sequence.append(nn.Linear(widths[i-1], widths[i]))
+            dense_sequence.append(eval(f'nn.{dense_activation}()'))
+
+        # create the last layer
+        dense_sequence.append(nn.Linear(widths[-1], self._conv_in[0] * self._conv_in[1] * self._conv_in[2]))
+
 
         # list convolution layers
         conv_sequence = []
-        if not conv_channels:
-            self._conv_out = input_height * input_width * input_channels
-        else:
-            channels = [input_channels] + conv_channels
+        if conv_channels:
+            channels = conv_channels + [output_channels]
             for i in range(1, len(channels)):
-
+                
                 # add a convolution layer
                 conv_sequence.append(nn.ConvTranspose2d(
-                    in_channels=channels[i - 1],
+                    in_channels=channels[i-1],
                     out_channels=channels[i],
                     kernel_size=conv_kernel,
                     padding=int(conv_kernel // 2)  # keeps the size as 'same'
                 ))
-
+                
                 # add an activation  
                 conv_sequence.append(eval(f'nn.{conv_activation}()'))
 
-                # add maxpool
+                # add upsampling
                 if i < len(channels) - 1:
-                    conv_sequence.append(nn.MaxPool2d(kernel_size=maxpool_kernel))
-                else:
-                    conv_sequence.append(nn.Flatten())
+                    conv_sequence.append(nn.UpsamplingNearest2d(size=conv_dimensions[i]))
 
-            # determine the number of outputs from the convolution layers
-            self._conv_out = input_height * input_width * channels[-1]
-            for i in range(len(conv_channels) - 1):
-                self._conv_out = self._conv_out // maxpool_kernel ** 2
-
-        # linear layers
-        dense_sequence = []
-        if dense_layers is None:
-            # default last layer
-            self.l_final = nn.Linear(self._conv_out, output_dim)
-        else:
-            widths = [self._conv_out] + dense_layers
-            for i in range(1, len(widths)):
-                # add a dense layer and activation
-                dense_sequence.append(nn.Linear(widths[i - 1], widths[i]))
-                dense_sequence.append(eval(f'nn.{dense_activation}()'))
-
-            # create the last layer
-            self.l_final = nn.Linear(widths[-1], output_dim)
 
         # put layers in sequential objects for use in self.forward()
         self.conv = nn.Sequential(*conv_sequence)
         self.dense = nn.Sequential(*dense_sequence)
-
+        
+        
     def forward(self, inputs):
-
-        # format for convolution
-        x = inputs.view(-1, self._channels, self._height, self._width)
-
-        # apply convolutions if any
-        if self.conv:
-            x = self.conv(x)
-
+                    
         # format for dense layers
-        x = x.view(-1, self._conv_out)
-
+        x = x.view(-1, self._input)
+        
         # apply dense layers if any
-        if self.dense:
-            x = self.dense(x)
+        x = self.dense(x)
 
-        # final layer
-        outputs = self.l_final(x)
-
+        # format for convolutions
+        x = inputs.view(-1, *self._conv_in)
+        
+        # apply convolutions if any
+        x = self.conv(x)
+        
         return outputs
